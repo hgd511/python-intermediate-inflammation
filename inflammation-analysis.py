2f58744fb5c7ceb221badbfc5d7cbb8edb0f8170
Harry Dudding <harry.dudding@ukaea.uk>
2023-11-10 13:24:47 +0000

    Code style fixes.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -19,17 +19,21 @@ def main(args):
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
 
-
     if args.full_data_analysis:
         analyse_data(os.path.dirname(InFiles[0]))
         return
 
-    for filename in InFiles:
+    for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {
+            'average': models.daily_mean(inflammation_data),
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)
+            }
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
